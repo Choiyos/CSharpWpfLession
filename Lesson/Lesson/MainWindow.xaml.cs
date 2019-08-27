dd9ffc11021bbfe16d85b0b8f981fcba27526d84
Choiyos <choiyos0417@gmail.com>
2019-08-27 18:14:43 +0900

    Lesson3 미완성 커밋
    패턴 여러가지 적용 기능 미완성. 8월 27일 마무리.

--- Lesson/Lesson/MainWindow.xaml.cs
+++ Lesson/Lesson/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Lesson
     /// </summary>
     public partial class MainWindow : Window
     {
+        int pattern = 1;
+        PatternSelectWindow patternSelectWindow = null;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,17 +31,60 @@ namespace Lesson
 
         private void BtnShow_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(txtbxInput.Text, out int inputNum) 
+
+            if (int.TryParse(txtbxInput.Text, out int inputNum)
                 && (inputNum > 0 && inputNum < 101))
             {
                 int sum = 0;
                 string star = string.Empty;
+                //lines가 홀수면 메시지 띄우기.
 
-                for (int i = 1; i <= inputNum; i++)
+                switch (pattern)
                 {
-                    sum += i;
-                    star = star.PadRight(sum, '*') + "\n";
-                    sum++;
+                    case 1:
+                        // 패턴1 출력.
+                        for (int i = 1; i <= inputNum; i++)
+                        {
+                            sum += i;
+                            star = star.PadRight(sum, '*') + "\n";
+                            sum++;
+                        }
+                        break;
+                    case 2:
+                        // 패턴2 출력.
+                        for (int i = inputNum; i >= 1; i--)
+                        {
+                            sum += i;
+                            star = star.PadRight(sum, '*') + "\n";
+                            sum++;
+                        }
+                        break;
+                    case 3:
+                        if (inputNum % 2 != 0)
+                        {
+                            // 다이아몬드모양 출력.
+                            for (int i = 1; i <= inputNum / 2; i++)
+                            {
+                                sum += i;
+                                star = star.PadRight(sum, '*') + "\n";
+                                sum++;
+                            }
+                            for (int i = (inputNum / 2)+1; i >= 1; i--)
+                            {
+                                sum += i;
+                                star = star.PadRight(sum, '*') + "\n";
+                                sum++;
+                            }
+                        }
+                        else
+                        {
+                            // 입력값이 짝수이므로 취소.
+                            MessageBox.Show("패턴 3은 홀수 라인만 입력 가능합니다.");
+                            return;
+                        }
+                        break;
+                    default:
+                        break;
                 }
 
                 txtDisplay.Text = star;
@@ -46,6 +93,33 @@ namespace Lesson
             {
                 MessageBox.Show("정상적인 숫자 범위를 입력해주세요(1~100).");
                 txtbxInput.Text = "1";
+            }
+        }
+
+
+        private void BtnPatternShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (patternSelectWindow == null)
+            {
+                patternSelectWindow = new PatternSelectWindow();
+                patternSelectWindow.OnChildSelectPatternEvent += new PatternSelectWindow.OnChildSelectPatternHander(psw_OnChildSelectPatternEvent);
+                patternSelectWindow.ShowDialog();
+            }
+        }
+
+        private void psw_OnChildSelectPatternEvent(string patternName)
+        {
+            if(int.TryParse(patternName.Substring(patternName.Length-1,1),out int parsedPattern))
+            {
+                pattern = parsedPattern;
+                txtPattern.Text = pattern.ToString();
+                if (patternSelectWindow != null)
+                {
+                    patternSelectWindow.Close();
+                    patternSelectWindow.OnChildSelectPatternEvent -= new PatternSelectWindow.OnChildSelectPatternHander(psw_OnChildSelectPatternEvent);
+                    patternSelectWindow = null;
+                }
+                MessageBox.Show(pattern.ToString());
             }
         }
     }
---
+++ Lesson/Lesson/PatternSelectWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lesson
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PatternSelectWindow : Window
+    {
+        public PatternSelectWindow()
+        {
+            InitializeComponent();
+        }
+        public delegate void OnChildSelectPatternHander(string patternName);
+        public event OnChildSelectPatternHander OnChildSelectPatternEvent;
+
+        private void BtnPatternSelect_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(RadioButton.IsCheckedProperty.Name);
+            if (OnChildSelectPatternEvent != null)
+            {
+                OnChildSelectPatternEvent(RadioButton.IsCheckedProperty.Name);
+            }
+        }
+    }
+}
