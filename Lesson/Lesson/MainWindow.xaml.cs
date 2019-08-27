0048ec66aa78b14087e13b03a32ea7fb8174be2d
Choiyos <choiyos0417@gmail.com>
2019-08-27 13:45:30 +0900

    별 삼각형 출력 소스파일 업로드

---
+++ Lesson/Lesson/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShow_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = 0;
+
+            string star = string.Empty;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int num = i;
+                sum += num;
+                star = star.PadRight(sum, '*')+"\n";
+                sum++;
+            }
+
+            MessageBox.Show(star);
+        }
+    }
+}
