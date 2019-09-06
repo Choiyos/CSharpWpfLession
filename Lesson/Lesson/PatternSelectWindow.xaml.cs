a44d80efb32f402a67cff500fe729af9d436a83b
Choiyos <choiyos0417@gmail.com>
2019-09-06 11:33:40 +0900

    라이브러리 참조하여 호출하도록 수정

--- Lesson/Lesson/PatternSelectWindow.xaml.cs
+++ Lesson/Lesson/PatternSelectWindow.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
+﻿using myPatternDLL;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Lesson
 {
@@ -25,6 +18,8 @@ namespace Lesson
 
         private List<TextBlock> _patternTextList = new List<TextBlock>();
 
+        private dllPattern pattern_dll = dllPattern.Instance;
+
         public PatternSelectWindow()
         {
             InitializeComponent();
@@ -32,18 +27,19 @@ namespace Lesson
             // xaml에 새로운 패턴을 만들 때에 Name속성의 Format을 맞춰주어야 누락되지 않음.
             _patternRadioButtonList = myGrid.Children.OfType<RadioButton>().Where(x => x.Name.Contains("rbPattern")).ToList();
             _patternTextList = myGrid.Children.OfType<TextBlock>().Where(x => x.Name.Contains("txtPattern")).ToList();
-        }
 
-        public event EventHandler<string> OnChildSelectPatternEvent;
+        }
 
         private void BtnPatternSelect_Click(object sender, RoutedEventArgs e)
         {
-            if (_rb is null)
+            if (pattern_dll.ChangePattern(_rb?.Content.ToString()))
+            {
+                Close();
+            }
+            else
             {
                 MessageBox.Show("선택된 패턴이 없습니다.");
             }
-            else
-                OnChildSelectPatternEvent?.Invoke(sender, _rb?.Content.ToString());
         }
 
         private void RadioButtonClicked(object sender, RoutedEventArgs e)
@@ -51,19 +47,11 @@ namespace Lesson
             _rb = (RadioButton)sender;
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            // 닫는 시점에 이벤트가 비어있지 않다면, Select 버튼을 클릭하지 않고 패턴 윈도우 닫기 버튼을 클릭한 것.
-            OnChildSelectPatternEvent?.Invoke(sender, string.Empty);
-        }
-
         private void TxtPattern_MouseDown(object sender, MouseButtonEventArgs e)
         {
             string txtBlock = (sender as TextBlock).Name;
 
-            // 라디오버튼List와 TextBlock의 순서가 동일하게 들어가야한다는 제한점이 있음. 
-            // xaml에서 TextBlock 작성 순서가 꼬이면 리스트에 들어가는 순서도 꼬일듯.
-            int raduiButtonIndex = _patternTextList.IndexOf(_patternTextList.Where( x => x == (TextBlock)sender).FirstOrDefault());
+            int raduiButtonIndex = _patternTextList.IndexOf(_patternTextList.Where(x => x == (TextBlock)sender).FirstOrDefault());
 
             _patternRadioButtonList[raduiButtonIndex].IsChecked = true;
             _rb = _patternRadioButtonList[raduiButtonIndex];
