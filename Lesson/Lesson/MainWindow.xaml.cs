5d2fab5dd598cfa801833528e5a10d69e7833277
Choiyos <choiyos0417@gmail.com>
2019-09-06 11:33:40 +0900

    라이브러리 참조하여 호출하도록 수정

--- Lesson/Lesson/MainWindow.xaml.cs
+++ Lesson/Lesson/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using myPatternDLL;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lesson
 {
@@ -20,18 +8,10 @@ namespace Lesson
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _pattern = 1;
 
         private PatternSelectWindow _patternSelectWindow = null;
 
-        private Dictionary<int, IPattern> _patternDic = new Dictionary<int, IPattern>
-        {
-            { 1, new FirstPattern() },
-            { 2, new SecondPattern() },
-            { 3, new ThirdPattern() },
-            { 4, new FourthPattern() },
-            { 5, new FifthPattern() }
-        };
+        private dllPattern _dllpattern = dllPattern.Instance;
 
         public MainWindow()
         {
@@ -40,53 +20,30 @@ namespace Lesson
 
         private void BtnShow_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(txtbxInput.Text, out int inputNum)
-                && (inputNum > 0 && inputNum < 101))
+            if (_dllpattern.Create(txtbxInput.Text))
             {
-                IPattern pattern = _patternDic[_pattern];
-
-                PatternModel patternModel = pattern?.Create(inputNum);
-
-                txtDisplay.Text = patternModel?.Content;
-                txtDisplay.TextAlignment = patternModel.TextAlignment;
+                txtDisplay.Text = _dllpattern.PatternResult;
+                txtDisplay.TextAlignment = _dllpattern.TextAlignment;
             }
             else
             {
-                MessageBox.Show("정상적인 숫자 범위를 입력해주세요(1~100).");
                 txtbxInput.Text = "1";
+                MessageBox.Show("1부터 100까지의 숫자만 입력 가능합니다.");
             }
         }
 
         private void BtnPatternShow_Click(object sender, RoutedEventArgs e)
         {
-            if (_patternSelectWindow == null)
-            {
-                _patternSelectWindow = new PatternSelectWindow();
-                _patternSelectWindow.OnChildSelectPatternEvent += new EventHandler<string>(psw_OnChildSelectPatternEvent);
-                _patternSelectWindow.Owner = this;
-                _patternSelectWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-                _patternSelectWindow.ShowDialog();
-            }
+            _patternSelectWindow = new PatternSelectWindow();
+            _patternSelectWindow.Closing += _patternSelectWindow_Closing;
+            _patternSelectWindow.Owner = this;
+            _patternSelectWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            _patternSelectWindow.ShowDialog();
         }
 
-        private void psw_OnChildSelectPatternEvent(object sender, string patternName)
+        private void _patternSelectWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (!string.IsNullOrEmpty(patternName) && int.TryParse(patternName.Substring(patternName.Length - 1, 1), out int parsedPattern))
-            {
-                _pattern = parsedPattern;
-                txtPattern.Text = patternName;
-                if (_patternSelectWindow != null)
-                {
-                    _patternSelectWindow.OnChildSelectPatternEvent -= new EventHandler<string>(psw_OnChildSelectPatternEvent);
-                    _patternSelectWindow.Close();
-                    _patternSelectWindow = null;
-                }
-            }
-            else
-            {
-                _patternSelectWindow.OnChildSelectPatternEvent -= new EventHandler<string>(psw_OnChildSelectPatternEvent);
-                _patternSelectWindow = null;
-            }
+            txtPattern.Text = _dllpattern.PatternName;
         }
     }
 }
